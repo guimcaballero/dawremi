31f5d62cf232f35e832c09d288ccf067988839c1
guimcaballero <guim.caballero@gmail.com>
2020-11-28 18:28:43 +0100

    Two different signals

--- src/song.rs
+++ src/song.rs
@@ -12,16 +12,25 @@ impl Song for Test {
         let one_sec = sample_rate as usize;
 
         // Create a signal chain to play back 1 second of each oscillator at A4.
-        let hz = signal::rate(sample_rate).const_hz(440.0);
-        let synth = hz
-            .clone()
+        let hz0 = signal::rate(sample_rate).const_hz(0.0);
+        let hz220 = signal::rate(sample_rate).const_hz(220.0);
+        let hz440 = signal::rate(sample_rate).const_hz(440.0);
+
+        // Create two signals that we'll merge
+        let synth1 = hz440.clone().sine().take(one_sec * 2);
+        let synth2 = hz0
             .sine()
             .take(one_sec)
-            .chain(saw(sample_rate, one_sec))
-            .chain(hz.clone().square().take(one_sec))
-            .chain(hz.clone().noise_simplex().take(one_sec))
-            .chain(signal::noise(0).take(one_sec))
-            .map(|s| s.to_sample::<f32>() * 0.2);
+            .chain(hz220.sine().take(one_sec * 2));
+        let signal1 = signal::from_iter(synth1);
+        let signal2 = signal::from_iter(synth2);
+
+        // Merge the signals into one
+        let synth = signal1
+            .zip_map(signal2, |a, b| (a + b) / 2.)
+            .map(|s| s.to_sample::<f32>() * 0.2)
+            .take(one_sec * 3);
+
         Box::new(synth)
     }
 }
