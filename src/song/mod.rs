597c6428b863321321da561466983b548b7e7264
guimcaballero <guim.caballero@gmail.com>
2020-12-03 17:26:01 +0100

    Add sound hashmap to song

--- src/song/mod.rs
+++ src/song/mod.rs
@@ -3,9 +3,10 @@ use dasp::{
     signal::{self, ConstHz},
     Sample, Signal,
 };
+use std::collections::HashMap;
 
-pub trait Song: HasSampleRate {
-    fn play(&self) -> Audio {
+pub trait Song: HasSampleRate + HasSoundHashMap {
+    fn play(&mut self) -> Audio {
         let mut tracks = some_vec![
             self.track1(),
             self.track2(),
@@ -65,7 +66,7 @@ pub trait Song: HasSampleRate {
         Box::new(synth)
     }
 
-    fn metronome(&self) -> Vec<f64> {
+    fn metronome(&mut self) -> Vec<f64> {
         if cfg!(feature = "metronome") {
             self.sound_signal("assets/metronome.wav")
                 .take(self.beats(0.2))
@@ -106,18 +107,28 @@ pub trait Song: HasSampleRate {
         let freq = freq.into();
         signal::rate(self.get_sample_rate()).const_hz(freq.0)
     }
-    fn sound(&self, path: &str) -> Vec<f64> {
-        let reader = hound::WavReader::open(path).unwrap();
 
-        reader
-            .into_samples::<i16>()
-            .filter_map(Result::ok)
-            .map(i16::to_sample::<f64>)
-            .collect()
+    fn sound(&mut self, path: &str) -> Vec<f64> {
+        let hashmap = self.get_sound_hashmap();
+        if let Some(vec) = hashmap.get(path) {
+            vec.to_vec()
+        } else {
+            let reader = hound::WavReader::open(path).unwrap();
+
+            let vec = reader
+                .into_samples::<i16>()
+                .filter_map(Result::ok)
+                .map(i16::to_sample::<f64>)
+                .collect::<Vec<f64>>();
+
+            hashmap.insert(path.to_string(), vec.clone());
+            vec
+        }
     }
-    fn sound_signal(&self, path: &str) -> signal::FromIterator<std::vec::IntoIter<f64>> {
+    fn sound_signal(&mut self, path: &str) -> signal::FromIterator<std::vec::IntoIter<f64>> {
         signal::from_iter(self.sound(path))
     }
+
     /// Returns the number of samples that should be taken to pass x seconds
     fn seconds(&self, x: f64) -> usize {
         (self.get_sample_rate() * x) as usize
@@ -146,11 +157,17 @@ pub trait HasSampleRate {
     fn get_sample_rate(&self) -> f64;
 }
 
+pub trait HasSoundHashMap {
+    /// Should only be called after setting sample_rate
+    fn get_sound_hashmap(&mut self) -> &mut HashMap<String, Vec<f64>>;
+}
+
 macro_rules! song {
     ($name:ident, $( $id:ident : $type:ty ),*) => {
         #[derive(Default)]
         pub struct $name {
             sample_rate: Option<f64>,
+            sound_hashmap: HashMap<String, Vec<f64>>,
             $( $id: $type )*
         }
 
@@ -160,6 +177,12 @@ macro_rules! song {
             }
             fn get_sample_rate(&self) -> f64 {
                 self.sample_rate.expect("Sample rate should have been set before playing the song")
+            }
+        }
+
+        impl HasSoundHashMap for $name {
+            fn get_sound_hashmap(&mut self) -> &mut HashMap<String, Vec<f64>>{
+                &mut self.sound_hashmap
             }
         }
     };
