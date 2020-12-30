9b64e8e31313314fe6ac40123368743d4aca1e55
guimcaballero <guim.caballero@gmail.com>
2020-12-30 11:41:24 +0100

    Add tracks to twinkle twinkle

--- src/song/twinkle_twinkle.rs
+++ src/song/twinkle_twinkle.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use super::*;
+use crate::effects::*;
 use crate::synth::*;
 
 // Twinkle Twinkle
@@ -17,21 +18,21 @@ impl Song for TwinkleTwinkle {
         "Twinkle Twinkle"
     }
     fn bpm(&self) -> usize {
-        120
+        240
     }
     fn duration(&self) -> usize {
         self.beats(8. * 6.)
     }
     fn tracks(&mut self) -> Vec<Vec<f64>> {
-        vec![self.track1(), self.track2()]
+        vec![self.main_track(), self.synth_track(), self.bass_track()]
     }
 }
 
 impl TwinkleTwinkle {
-    fn track1(&self) -> Vec<f64> {
+    fn main_track(&self) -> Vec<f64> {
         sequence!(@lyrics
                 self,
-                len: 0.5,
+                len: 1.,
                 fun: |note| self.plucked(note),
 
                 [twin-kle  twin-kle  lit-tle star],
@@ -45,24 +46,37 @@ impl TwinkleTwinkle {
         )
     }
 
-    fn track2(&self) -> Vec<f64> {
-        let sign = CustomSignal {
-            sample: 0,
-            sample_rate: self.get_sample_rate(),
-        };
+    fn bass_track(&self) -> Vec<f64> {
+        sequence!(@lyrics
+                  self,
+                  len: 1., fun: |note| self.bass(note),
+
+                  [twin-kle  twin-kle  lit-tle star],
+                  (G4 __ G4 __ D4 __ D4 __ E4 __ E4 __ (D4 * 2.) __ __),
+                  [how  I    won-der  how  you  are],
+                  (C4 __ C4 __ B4 __ B4 __ A4 __ A4 __ (G4 * 2.) __ __),
+                  (D4 __ D4 __ C4 __ C4 __ B4 __ B4 __ (A4 * 2.) __ __),
+                  (D4 __ D4 __ C4 __ C4 __ B4 __ B4 __ (A4 * 2.) __ __),
+                  (G4 __ G4 __ D4 __ D4 __ E4 __ E4 __ (D4 * 2.) __ __),
+                  (C4 __ C4 __ B4 __ B4 __ A4 __ A4 __ (G4 * 2.) __ __),
+        )
+    }
+
+    fn synth_track(&self) -> Vec<f64> {
         sequence!(@lyrics
                 self,
-                len: 0.5, signal: sign,
+                len: 1., fun: |note| self.synth(note),
 
                 [twin-kle  twin-kle  lit-tle star],
-                (x _ x _ x _ x _ x _ x _ x x _ _),
+                (G4 __ G4 __ D4 __ D4 __ E4 __ E4 __ (D4 * 2.) __ __),
                 [how  I    won-der  how  you  are],
-                (x _ x _ x _ x _ x _ x _ x x _ _),
-                (x _ x _ x _ x _ x _ x _ x x _ _),
-                (x _ x _ x _ x _ x _ x _ x x _ _),
-                (x _ x _ x _ x _ x _ x _ x x _ _),
-                (x _ x _ x _ x _ x _ x _ x x _ _),
+                (C4 __ C4 __ B4 __ B4 __ A4 __ A4 __ (G4 * 2.) __ __),
+                (D4 __ D4 __ C4 __ C4 __ B4 __ B4 __ (A4 * 2.) __ __),
+                (D4 __ D4 __ C4 __ C4 __ B4 __ B4 __ (A4 * 2.) __ __),
+                (G4 __ G4 __ D4 __ D4 __ E4 __ E4 __ (D4 * 2.) __ __),
+                (C4 __ C4 __ B4 __ B4 __ A4 __ A4 __ (G4 * 2.) __ __),
         )
+        .effect(&Volume { mult: 0.5 })
     }
 
     fn synth(&self, note: Note) -> Synth {
@@ -75,6 +89,17 @@ impl TwinkleTwinkle {
     fn plucked(&self, note: Note) -> Synth {
         Synth::new(
             box Plucked::new(InitialBurstType::Random, note, self.get_sample_rate()),
+            note,
+            self.get_sample_rate(),
+        )
+    }
+    fn bass(&self, note: Note) -> Synth {
+        Synth::new(
+            box Plucked::new(
+                InitialBurstType::DoubleTriangle,
+                note,
+                self.get_sample_rate(),
+            ),
             note,
             self.get_sample_rate(),
         )
