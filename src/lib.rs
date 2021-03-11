410a943f1fba5103d0dea6a21d09e809d7a352b5
guimcaballero <guim.caballero@gmail.com>
2021-03-11 18:54:39 +0100

    Clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity
+)]
+#![allow(clippy::wildcard_imports, clippy::enum_glob_use)]
+#![warn(clippy::wrong_pub_self_convention, clippy::unseparated_literal_suffix)]
+
 pub use dawremi_core::*;
