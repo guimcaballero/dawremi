177142a474e7906b3dbc48738b1128f90a1b85a5
guimcaballero <guim.caballero@gmail.com>
2021-03-27 16:40:36 +0100

    Rework pitch shift and prepare for Autotune rework

--- crates/dawremi_core/src/effects/autotune.rs
+++ crates/dawremi_core/src/effects/autotune.rs
@@ -1,8 +1,6 @@
 use super::*;
 use crate::notes::*;
 
-use pvoc::{Bin, PhaseVocoder};
-
 pub struct Autotune {
     pub sample_rate: f64,
 
@@ -24,6 +22,11 @@ impl Effect for Autotune {
                 // TODO Currently there's no pitch detection, it just scales it assuming it's a C4
                 // TODO Implement pitch detection to check the actual pitch
 
+                // TODO Get actual pitch for each side
+                // TODO Find closest note
+                // TODO Calculate the shift needed
+                // TODO Pitch shift each section
+
                 let freq: Frequency = if let Some(note) = note {
                     note.into()
                 } else {
@@ -33,59 +36,10 @@ impl Effect for Autotune {
                 let c4f: Frequency = Note::C4.into();
                 let shift = freq.0 / c4f.0;
 
-                let output_left = {
-                    let mut pvoc = PhaseVocoder::new(1, self.sample_rate, 256, 4);
-                    let mut output = vec![0.0; chunk.len()];
-
-                    pvoc.process(
-                        &[&chunk.left()],
-                        &mut [&mut output],
-                        |channels: usize,
-                         bins: usize,
-                         input: &[Vec<Bin>],
-                         output: &mut [Vec<Bin>]| {
-                            for i in 0..channels {
-                                for j in 0..bins / 2 {
-                                    let index = ((j as f64) * shift) as usize;
-                                    if index < bins / 2 {
-                                        output[i][index].freq = input[i][j].freq * shift;
-                                        output[i][index].amp += input[i][j].amp;
-                                    }
-                                }
-                            }
-                        },
-                    );
-
-                    output
-                };
-
-                let output_right = {
-                    let mut pvoc = PhaseVocoder::new(1, self.sample_rate, 256, 4);
-                    let mut output = vec![0.0; chunk.len()];
-
-                    pvoc.process(
-                        &[&chunk.right()],
-                        &mut [&mut output],
-                        |channels: usize,
-                         bins: usize,
-                         input: &[Vec<Bin>],
-                         output: &mut [Vec<Bin>]| {
-                            for i in 0..channels {
-                                for j in 0..bins / 2 {
-                                    let index = ((j as f64) * shift) as usize;
-                                    if index < bins / 2 {
-                                        output[i][index].freq = input[i][j].freq * shift;
-                                        output[i][index].amp += input[i][j].amp;
-                                    }
-                                }
-                            }
-                        },
-                    );
-
-                    output
-                };
-
-                join_left_and_right_channels(output_left, output_right)
+                chunk.to_vec().effect(&PitchShift {
+                    sample_rate: self.sample_rate,
+                    shift: Frame::mono(shift),
+                })
             })
             .flatten()
             .collect::<Vec<Frame>>()
--- crates/dawremi_core/src/effects/pitch_shift.rs
+++ crates/dawremi_core/src/effects/pitch_shift.rs
@@ -6,21 +6,23 @@ use pvoc::{Bin, PhaseVocoder};
 
 pub struct PitchShift {
     pub sample_rate: f64,
-    pub shift: f64,
+    pub shift: Frame,
 }
 
 impl Effect for PitchShift {
     fn run(&self, input: Vec<Frame>) -> Vec<Frame> {
         let (left, right) = input.split_sides();
 
-        join_left_and_right_channels(run(self, left), run(self, right))
+        join_left_and_right_channels(
+            run(self, left, self.shift.left),
+            run(self, right, self.shift.right),
+        )
     }
 }
 
-fn run(pitch: &PitchShift, input: Vec<f64>) -> Vec<f64> {
+fn run(pitch: &PitchShift, input: Vec<f64>, shift: f64) -> Vec<f64> {
     let mut pvoc = PhaseVocoder::new(1, pitch.sample_rate, 256, 4);
     let mut output = vec![0.0; input.len()];
-    let shift = pitch.shift;
 
     pvoc.process(
         &[&input],
