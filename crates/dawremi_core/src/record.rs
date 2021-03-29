605e7c989482d579686db3291a9bb7bff28c46f0
guimcaballero <guim.caballero@gmail.com>
2021-03-29 23:20:47 +0200

    Rework recorder

--- crates/dawremi_core/src/record.rs
+++ crates/dawremi_core/src/record.rs
@@ -1,24 +1,14 @@
 //! Record uses cpal under the hood to record audio
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use std::fs::File;
 use std::io::stdin;
-use std::io::BufWriter;
 use std::sync::{Arc, Mutex};
 
-pub fn main() -> Result<(), anyhow::Error> {
-    // Use the default host for working with audio devices.
-    let host = cpal::default_host();
+use crate::frame::Frame;
+use crate::sound_files::io::save_file;
 
-    // Setup the default input device and stream with the default input config.
-    let device = host
-        .default_input_device()
-        .expect("Failed to get default input device");
-    println!("Default input device: {}", device.name()?);
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
-    println!("Default input config: {:?}", config);
+pub fn record_and_save() {
+    let audio = record_input().expect("Input could not be recorded");
 
     let time = {
         use std::time::{SystemTime, UNIX_EPOCH};
@@ -31,35 +21,75 @@ pub fn main() -> Result<(), anyhow::Error> {
     };
 
     // The WAV file we're recording to.
-    let path = format!("output/recorded/recorded_{}.wav", time);
-    let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(&path, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
+    let path = format!("assets/recorded/recorded_{}.wav", time);
 
-    // A flag to indicate that recording is in progress.
+    // NOTE We're grabbing the config again, we should probably try to reuse it
+    // Specially when we allow selecting different devices
+
+    let host = cpal::default_host();
+    let device = host
+        .default_input_device()
+        .expect("Failed to get default input device");
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+    let stream_config: cpal::StreamConfig = config.into();
+    let sample_rate = stream_config.sample_rate.0;
+    let bits_per_sample = 24;
+
+    println!("File saved: {}", &path);
+    save_file(audio, &path, sample_rate, bits_per_sample);
+}
+
+pub fn record_input() -> Result<Vec<Frame>, anyhow::Error> {
+    // Use the default host for working with audio devices.
+    let host = cpal::default_host();
+
+    // Setup the default input device and stream with the default input config.
+    let device = host
+        .default_input_device()
+        .expect("Failed to get default input device");
+    println!("Default input device: {}", device.name()?);
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+
+    println!("Press q to start recording");
+    loop {
+        let mut s = String::new();
+        stdin()
+            .read_line(&mut s)
+            .expect("Did not enter a correct string");
+        if s.trim() == "q" {
+            break;
+        }
+    }
     println!("Begin recording...");
 
-    // Run the input stream on a separate thread.
-    let writer_2 = writer.clone();
+    let vec: Vec<Frame> = vec![];
+    let vec = Arc::new(Mutex::new(vec));
+    let vec2 = Arc::clone(&vec);
 
     let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
+        println!("an error occurred on stream: {}", err);
     };
+
+    let channels = config.channels();
 
     let stream = match config.sample_format() {
         cpal::SampleFormat::F32 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
+            move |data, _: &_| write_input_data::<f32>(data, &vec, channels),
             err_fn,
         )?,
         cpal::SampleFormat::I16 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
+            move |data, _: &_| write_input_data::<i16>(data, &vec, channels),
             err_fn,
         )?,
         cpal::SampleFormat::U16 => device.build_input_stream(
             &config.into(),
-            move |data, _: &_| write_input_data::<u16, i16>(data, &writer_2),
+            move |data, _: &_| write_input_data::<u16>(data, &vec, channels),
             err_fn,
         )?,
     };
@@ -79,40 +109,21 @@ pub fn main() -> Result<(), anyhow::Error> {
     }
 
     drop(stream);
-    writer.lock().unwrap().take().unwrap().finalize()?;
-    println!("Recording {} complete!", path);
-    Ok(())
+    println!("Recording complete!");
+
+    Ok(Arc::try_unwrap(vec2).unwrap().into_inner().unwrap())
 }
 
-fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
-    match format {
-        cpal::SampleFormat::U16 => hound::SampleFormat::Int,
-        cpal::SampleFormat::I16 => hound::SampleFormat::Int,
-        cpal::SampleFormat::F32 => hound::SampleFormat::Float,
-    }
-}
-
-fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
-    hound::WavSpec {
-        channels: config.channels() as _,
-        sample_rate: config.sample_rate().0 as _,
-        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
-        sample_format: sample_format(config.sample_format()),
-    }
-}
-
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
-
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
+fn write_input_data<T>(input: &[T], vec: &Arc<Mutex<Vec<Frame>>>, channels: u16)
 where
-    T: cpal::Sample,
-    U: cpal::Sample + hound::Sample + std::fmt::Debug,
+    T: cpal::Sample + std::fmt::Debug,
 {
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                let sample: U = cpal::Sample::from(&sample);
-                writer.write_sample(sample).ok();
+    if let Ok(mut vec) = vec.try_lock() {
+        for frame in input.chunks(channels.into()) {
+            match frame {
+                [left, right] => vec.push(Frame::new(left.to_f32() as f64, right.to_f32() as f64)),
+                [a] => vec.push(Frame::mono(a.to_f32() as f64)),
+                _ => {}
             }
         }
     }
