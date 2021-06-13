cf2921338e2892a8dd0f39e166f50329c82eb363
guimcaballero <guim.caballero@gmail.com>
2021-06-13 13:07:52 +0200

    Add music theory example

---
+++ examples/music_theory.rs
@@ -0,0 +1,48 @@
+//! Dawremi has some support of traditional western music theory
+//! This is a demonstration of how it works
+
+use dawremi::prelude::*;
+
+fn main() {
+    let config = SongConfig {
+        name: "Music theory".to_string(),
+        bpm: 120.,
+        ..Default::default()
+    };
+    let mut song = Song::new(vec![plucked_track.into()], config);
+    song.play().expect("Unable to play song");
+}
+
+fn guitar(song: &Song, frequency: Frequency, length: usize, burst: InitialBurstType) -> Vec<Frame> {
+    Plucked(burst).generate(
+        length,
+        frequency,
+        song.sample_rate(),
+        Plucked::default_adsr(song.sample_rate()),
+    )
+}
+
+fn plucked_track(song: &Song) -> Vec<Frame> {
+    let notes = {
+        use Note::*;
+        [
+            (C4 >> Chord::Major).beats(1.),
+            (F4 >> Chord::Minor).beats(1.),
+            (F4 >> Chord::DominantSeventh).beats(1.),
+            Silence.beats(4.),
+            (C4 >> Chord::Major).beats(1.),
+            (F4 >> Chord::Minor).beats(1.),
+            (F4 >> Chord::DominantSeventh).beats(1.),
+        ]
+    };
+
+    notes
+        .generate(
+            song,
+            // Function to use to generate the audio
+            &mut |note, length| guitar(song, note, length, InitialBurstType::Triangle(2, 3)),
+        )
+        .effect(&Volume {
+            mult: Automation::Const(0.3),
+        })
+}
